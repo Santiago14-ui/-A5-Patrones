fc4cf679feac51531488202f0cf8a41c63305c55
Santiago14-ui <santiagoa.deltoroc@uqvirtual.edu.co>
2026-09-16 23:12:19 -0500

    configuracionJuegoEnLinea terminado

--- configuracionJuegoEnLinea/configuracionJuegoEnLinea/src/main/java/org/example/Main.java
+++ configuracionJuegoEnLinea/configuracionJuegoEnLinea/src/main/java/org/example/Main.java
@@ -1,17 +1,36 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) {
+
+        // Los dos jugadores acceden a la misma configuración
+        ConfiguracionJuego configuracion1 =
+                ConfiguracionJuego.getInstancia();
+
+        ConfiguracionJuego configuracion2 =
+                ConfiguracionJuego.getInstancia();
+
+        System.out.println("Configuración inicial:");
+        configuracion1.mostrarConfiguraciones();
+
+        // Cambiamos una configuración desde el jugador 1
+        configuracion1.cambiarConfiguracion("Dificultad", "Facil");
+
+        System.out.println("\nDespués de cambiar la dificultad:");
+        configuracion2.mostrarConfiguraciones();
+
+        // Agregamos una nueva configuración
+        configuracion1.agregarConfiguracion("Resolución", "1920x1080");
+
+        System.out.println("\nConfiguración final:");
+        configuracion1.mostrarConfiguraciones();
+
+
+        if (configuracion1 == configuracion2) {
+            System.out.println("\nLos dos jugadores usan la misma configuración.");
+        } else {
+            System.out.println("\nLos jugadores tienen configuraciones diferentes.");
         }
     }
 }
